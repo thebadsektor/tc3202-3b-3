769e0b04870ef8def46a893bc5e31413871bf06c
genblitzz07 <delosreyesgenesis551@gmail.com>
2025-05-13 01:19:25 +0800

    Instructions

--- PoliticalViewRS/src/data/instructionsSlides.jsx
+++ PoliticalViewRS/src/data/instructionsSlides.jsx
@@ -1,27 +1,51 @@
+import one from "../assets/InstructionPicss/1.png";
+import two from "../assets/InstructionPicss/2.png";
+import three from "../assets/InstructionPicss/3.png";
+import four from "../assets/InstructionPicss/4.png";
+import five from "../assets/InstructionPicss/5.png";
+import six from "../assets/InstructionPicss/6.png";
 const slides = [
   {
-    image:
-      "https://www.rappler.com/tachyon/2025/01/ABALOS-BENHUR.jpg?fit=300%2C300&resize=200%2C200",
+    image: one,
     english:
-      "The system will generate two choices at a time and you have to pick one choice until you accomplish the test.",
+      "Upon starting the test, the system will generate two choices at a time and you have to pick one option of your choice until you accomplish each set.",
     tagalog:
-      "Bibigyan ka ng sistema ng dalawang pagpipilian sa bawat pagkakataon at kailangan mong pumili ng isa hanggang sa matapos mo ang pagsusulit.",
+      "Sa pag simula ng test, Bibigyan ka ng sistema ng dalawang pagpipilian sa bawat pagkakataon at kailangan mong pumili ng isa na iyong nain hanggang sa matapos mo ang bawat set.",
   },
   {
-    image:
-      "https://www.rappler.com/tachyon/2025/01/ABALOS-BENHUR.jpg?fit=300%2C300&resize=200%2C200",
+    image: two,
     english:
-      "After choosing, the system will record your preference for analysis.",
+      "After you are done with each set of questions, the system will conduct a simple survey about your information like your Age and Gender",
     tagalog:
-      "Matapos pumili, itatala ng sistema ang iyong pagpili para sa pagsusuri.",
+      "Matapos pumili, itatala ng sistema ang iyong pagpili para sa Pagkatapos mong matapos ang bawat set ng mga tanong, ang sistema ay magsasagawa ng isang simpleng survey tungkol sa iyong impormasyon tulad ng iyong Edad at Kasarian.",
   },
   {
-    image:
-      "https://www.rappler.com/tachyon/2025/01/ABALOS-BENHUR.jpg?fit=300%2C300&resize=200%2C200",
+    image: three,
     english:
-      "When all choices are done, your result will be shown based on your selections.",
+      "After you are done answering, your predicted values will be shown in the result.",
     tagalog:
-      "Kapag natapos na ang lahat ng pagpili, ipapakita ang resulta base sa iyong mga piniling sagot.",
+      "Pagkatapos mong sumagot, ipapakita sa resulta ang iyong mga inaasahang halaga o prediksyon.",
+  },
+  {
+    image: four,
+    english:
+      "By scrolling down, you will see what the system provided you with the candidates that fits you based on your answers and you can view them by clicking on the profile button.",
+    tagalog:
+      "Sa pag-scroll pababa, makikita mo ang mga kandidato na ibinigay ng sistema na akma sa iyong mga sagot, at maaari mo silang tingnan sa pamamagitan ng pag-click sa Profile na button.",
+  },
+  {
+    image: five,
+    english:
+      "Upon clicking, you will be able to see the informations of the politician",
+    tagalog:
+      "Kapag na-click mo ito, makikita mo ang mga impormasyon tungkol sa politiko.",
+  },
+  {
+    image: six,
+    english:
+      "from the recommended politicians, by scrolling further more you will see the articles related to the recommended politicians.",
+    tagalog:
+      "Mula sa mga inirekumendang politiko, sa pag-scroll mo pa pababa, makikita mo ang mga artikulong may kaugnayan sa kanila.",
   },
 ];
 
